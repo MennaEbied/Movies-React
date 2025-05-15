8bc3cb907470eb8ebf4d716188e36c825b836ce4
MennaEbied <mennahesham253@gmail.com>
2025-05-16 00:57:34 +0300

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
 function Footer() {
   return (
-    <div className="bottom-0 py-4 px-6  bg-dark text-Orange grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="bottom-0 py-4 px-6  bg-dark text-Orange grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pt-7">
       <p className="font-bold">TVmov</p>
       <div className="flex flex-col">
-        <p>Help</p>
-        <a href="https://www.themoviedb.org/about/get-in-touch/">Contact Us</a>
+        <h3 className="font-bold">The Basics</h3>
+        <a
+          href="https://www.themoviedb.org/about/get-in-touch/"
+          className="hover:text-orange-300 transition-colors"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Us
+        </a>
         <a
           href="https://www.themoviedb.org/about "
           target="_blank"
@@ -16,9 +23,13 @@ function Footer() {
         <p>Features</p>
       </div>
       <div className="flex flex-col">
-        <p>Documentation</p>
+        <a href="https://developer.themoviedb.org/docs/getting-started" target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-orange-300 transition-colors">Documentation</a>
         <p>FAQ</p>
-        <p>Support</p>
+        <a href="https://www.themoviedb.org/talk" target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-orange-300 transition-colors">Support</a>
       </div>
       <div>
         <p> &copy; {new Date().getFullYear()} TVmov. All rights reserved. </p>
